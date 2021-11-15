2f3ce05dce04b1068858e20882b3d7d6fa10f4ce
Attila Patel <attila_patel@hotmail.com>
2021-11-15 15:43:30 +0000

    changed tier

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "azurerm_app_service_plan" "plan-app" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Free"
+    size = "F1"
   }
 }
 
